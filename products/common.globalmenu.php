bdd8d6c0b1938ce6393aca9d9b78606c985f949e
kuchida <kuchita@6web.co.jp>
2015-04-27 22:14:47 +0900

    バグ修正　refs #126

--- products/common.globalmenu.php
+++ products/common.globalmenu.php
@@ -1,8 +1,11 @@
-<li id="gn1"><a href="/products/">ホーム</a></li>
-<li id="gn2"><a href="/products/parent/?parent_id=1">浴室水栓</a></li>
-<li id="gn3"><a href="/products/parent/?parent_id=2">キッチン用水栓</a></li>
-<li id="gn4"><a href="/products/parent/?parent_id=3">洗面用水栓</a></li>
-<li id="gn5"><a href="/products/parent/?parent_id=4">給水栓</a></li>
-<li id="gn6"><a href="/products/parent/?parent_id=5">継手・配管部材</a></li>
-<li id="gn7"><a href="/products/parent/?parent_id=6">パーツ</a></li>
-<li id="gn8"><a href="../contact/index.html" >お問い合わせ</a></li>
+
+
+<?php
+
+
+	echo "<li id=\"gn1\"><a href=\"/products/\">ホーム</a></li>";
+	
+	echo $cls_product->getGlobalMenu();
+
+	echo "<li id=\"gn8\"><a href= \"".$_SERVER[“SERVER_NAME”]."/contact/index.html\" ?>>お問い合わせ</a></li>";
+?>
--- products/common.sidemenu.php
+++ products/common.sidemenu.php
@@ -19,17 +19,17 @@ echo $cls_product->getSidemenu( $key,$value );
 
 <h2 class="sideguide"><strong>ガイド</strong></h2>
 <dl>
-<dt><a href= "/e_catalog/search/index.html" >商品検索の方法</a></dt>
-<dt><a href= "/e_catalog/index.html" >電子カタログ</a></dt>
-<dt><a href= "/e_catalog/oldsearch/index.html" >以前の商品</a></dt>
-<dt><a href= "/e_catalog/point/index.htm" >水栓の仕組み</a></dt>
-<dt><a href= "/e_catalog/word/index.html" >用語集</a></dt>
-<dt><a href= "/support/maintenance/index.html" >メンテナンス情報</a></dt>
-<dt><a href= "/support/professional/index.html" >業者様向けメンテナンス情報</a></dt>
-<dt><a href= "/support/maintenance/faq/index.html" >よくあるご質問（Q＆A）</a></dt>
-<dt><a href= "/support/example/index.html" >水まわりのご提案</a></dt>
-<dt><a href= "/support/reform/index.htm" >リフォーム水栓</a></dt>
-<dt><a href= "/support/maintenance/after/index.html" >水栓のお手入れ方法</a></dt>
-<dt><a href= "/support/shop/parts.html" >主要部品一覧</a></dt>
-<dt><a href= "/corporate/map/network.html" >お近くの支社・営業所</a></dt>
+<dt><a href= <?php echo "/e_catalog/search/index.html" ?>>商品検索の方法</a></dt>
+<dt><a href= <?php echo "/e_catalog/index.html" ?>>電子カタログ</a></dt>
+<dt><a href= <?php echo "/e_catalog/oldsearch/index.html" ?>>以前の商品</a></dt>
+<dt><a href= <?php echo "/e_catalog/point/index.htm" ?>>水栓の仕組み</a></dt>
+<dt><a href= <?php echo "/e_catalog/word/index.html" ?>>用語集</a></dt>
+<dt><a href= <?php echo "/support/maintenance/index.html" ?>>メンテナンス情報</a></dt>
+<dt><a href= <?php echo "/support/professional/index.html" ?>>業者様向けメンテナンス情報</a></dt>
+<dt><a href= <?php echo "/support/maintenance/faq/index.html" ?>>よくあるご質問（Q＆A）</a></dt>
+<dt><a href= <?php echo "/support/example/index.html" ?>>水まわりのご提案</a></dt>
+<dt><a href= <?php echo "/support/reform/index.htm" ?>>リフォーム水栓</a></dt>
+<dt><a href= <?php echo "/support/maintenance/after/index.html" ?>>水栓のお手入れ方法</a></dt>
+<dt><a href= <?php echo "/support/shop/parts.html" ?>>主要部品一覧</a></dt>
+<dt><a href= <?php echo "/corporate/map/network.html" ?>>お近くの支社・営業所</a></dt>
 </dl>
